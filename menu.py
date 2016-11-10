af5df327de1272a614565726fbd92c766f8b55f6
tomek <tomekzegota@hgmail.com>
2016-11-10 17:23:49 +0100

    1

--- menu.py
+++ menu.py
@@ -13,33 +13,65 @@ screen_size = (500,600)      # ustalamy rozmiar ekranu
 class menu(object):
 
     def __init__(self):
+        pygame.init()  
+        flag = DOUBLEBUF    # wlaczamy tryb podwojnego buforowania
+        
+        # tworzymy bufor na  grafike
+        self.surface = pygame.display.set_mode(screen_size,flag)
         #self.myfont = pygame.font.SysFont("monospace", 16)
         self.gamestate=1
-        self.basicFont=pygame.font.SysFont(None, 30)
         
-        start = self.basicFont.render("start", 1, (0,255,255))
-        ustawienia=self.basicFont.render("ustawienia", 1, (0,255,255))
-                                         
-        rekordy=self.basicFont.render("rekordy= ", 1, (0,255,255))
-        exit=self.basicFont.render("exit= ", 1, (0,255,255))
-        self.surface.blit(start,punkty.get_rect())
-        self.surface.blit(ustawienia,punkty.get_rect())
-        self.surface.blit(rekordy,punkty.get_rect())
-        self.surface.blit(exit,punkty.get_rect())
-        pygame.display.flip() 
+        self.basicFont = pygame.font.SysFont(None, 30)
         
+        self.pozycja=1
+       
+        self.color1=(255,255,255)
+        self.color0=(0,255,255)
         self.loop()
-    def loop():
-    
-    
+    def loop(self):
+ 
         
+        
+    
+        keys = pygame.key.get_pressed() # odczytujemy stan klawiszy
+        if keys[pygame.K_UP]:
+            self.pozycja-=1
+        if keys[pygame.K_DOWN]:
+            self.pozycja+=1     
+
+        if self.pozycja == 1:
+            start = self.basicFont.render("start", 1, self.color0)
+        else:
+            start = self.basicFont.render("start", 1, self.color1)
+        if self.pozycja == 2:
+            ustawienia=self.basicFont.render("ustawienia", 1, self.color0)
+        else:
+            ustawienia=self.basicFont.render("ustawienia", 1, self.color1)
+        if self.pozycja== 3:
+            rekordy=self.basicFont.render("rekordy", 1, self.color0)
+        else:
+            rekordy=self.basicFont.render("rekordy", 1, self.color1)
+        if self.pozycja== 4:
+            exit=self.basicFont.render("exit", 1, self.color0)
+        else:
+            exit=self.basicFont.render("exit", 1, self.color1)
+        
+        
+        
+        
+        
+        self.surface.blit(start,(250,100))
+        self.surface.blit(ustawienia,(250,150))
+        self.surface.blit(rekordy, (250,200))
+        self.surface.blit(exit,(250,250))
+        pygame.display.flip() 
         if self.gamestate ==0:
             pygame.display.quit()
             sys.exit(0) 
             pygame.quit()
             self.game_exit()
     
-        self.game_exit()    
+        #self.game_exit()    
 if __name__ == '__main__':
    menu()
     
